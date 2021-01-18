db754cb4ebc96e34e745a795397bd3974d95d874
yunqian44 <yunqian8@live.com>
2021-01-18 14:18:13 +0800

    Test terraform version

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "0.12.3"
+  #required_version = "0.12.3"
   backend "azurerm" {
     storage_account_name = "cnbateterraformstorage"
     container_name       = "terraform-state"
