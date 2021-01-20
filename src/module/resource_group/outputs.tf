864d310e2dca202ac23cedc37638cc663d568367
yunqian44 <yunqian8@live.com>
2021-01-20 18:06:35 +0800

    add resource_group module

---
+++ src/module/resource_group/outputs.tf
@@ -0,0 +1,8 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.resource_group.name
+}
+
+output "location" {
+  value = azurerm_resource_group.resource_group.location
+}
+
---
+++ src/module/resource_group/variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group_name" {
+  description="The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
+}
+
+variable "location" {
+  description="The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
+}
