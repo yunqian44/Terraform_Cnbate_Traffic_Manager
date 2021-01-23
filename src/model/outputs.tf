9d121a609009d648946bf00c36ec6372f7611fee
yunqian44 <yunqian8@live.com>
2021-01-24 03:03:28 +0800

    add outputs

---
+++ src/model/outputs.tf
@@ -0,0 +1,17 @@
+output "cnbate_traffic_manager_id" {
+  value = module.cnbate_traffic_manager.traffic_manager_profile_id
+}
+
+output "cnbate_traffic_manager_name" {
+  value = module.cnbate_traffic_manager.traffic_manager_profile_name
+}
+
+output "cnbate_Web_app_ids" {
+  value = module.cnbate_Web_app.azurerm_app_service_ids
+}
+
+output "cnbate_Web_app_names" {
+  value = module.cnbate_Web_app.azurerm_app_service_names
+}
+
+
