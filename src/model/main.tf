4569cd5dbdffb9074772ca4edb8a71f18b402d83
yunqian44 <yunqian8@live.com>
2021-01-24 03:03:19 +0800

    add model

--- src/model/main.tf
+++ src/model/main.tf
@@ -1,114 +1,56 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "cnbateterraformstorage"
-    container_name       = "terraform-state"
-    key                  = "cnbate.terraform.stats"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     storage_account_name = "cnbateterraformstorage"
+#     container_name       = "terraform-state"
+#     key                  = "cnbate.terraform.stats"
+#   }
+# }
 
 provider "azurerm" {
   version = "~>2.0"
   features {}
 }
 
-data "azurerm_resource_group" "cnbate_resource_group" {
-  name = "Web_Test_TF_RG"
-}
-
-
 locals {
   location_eastAsia      = "East Asia"
   location_southeastAsia = "Southeast Asia"
 }
 
-resource "azurerm_traffic_manager_profile" "cnbate_traffic_manager_profile" {
-  name                = "cnbateblogweb"
-  resource_group_name = data.azurerm_resource_group.cnbate_resource_group.name
+data "azurerm_resource_group" "cnbate_resource_group" {
+  name = var.resource_group_name
 
-  traffic_routing_method = "Geographic"
-
-  dns_config {
-    relative_name = "cnbateblogweb"
-    ttl           = 60
-  }
-
-  monitor_config {
-    protocol                     = "http"
-    port                         = 80
-    path                         = "/"
-    interval_in_seconds          = 30
-    timeout_in_seconds           = 10
-    tolerated_number_of_failures = 3
-  }
-
-  tags = {
-    Environment = "Production"
-  }
 }
 
-resource "azurerm_traffic_manager_endpoint" "cnbate_traffic_manager_endpoint01" {
-  name                = "cnbateblogweb_webapp01_performance"
-  resource_group_name = data.azurerm_resource_group.cnbate_resource_group.name
-  profile_name        = azurerm_traffic_manager_profile.cnbate_traffic_manager_profile.name
-  target_resource_id  = azurerm_app_service.cnbate_app_service01.id
-  type                = "azureEndpoints"
+module "cnbate_traffic_manager" {
+  source                 = "../module/traffic_manager_profile"
+  traffic_manager_name   = var.traffic_manager_name
+  resource_group_name    = data.azurerm_resource_group.cnbate_resource_group.name
+  traffic_routing_method = var.traffic_routing_method
+  relative_name          = var.relative_name
+  ttl                    = var.ttl
+  tags                   = var.tags
 
-  geo_mappings = ["CN"]
+  enable_traffic_manager_endpoint = var.enable_traffic_manager_endpoint
+  traffic_manager_endpoint_count  = var.traffic_manager_endpoint_count
+  traffic_manager_endpoint_names  = var.traffic_manager_endpoint_names
+  target_resource_ids             = module.cnbate_Web_app.azurerm_app_service_ids
+  traffic_manager_endpoint_type   = var.traffic_manager_endpoint_type
+  geo_mappings                    = var.geo_mappings
 }
 
-resource "azurerm_traffic_manager_endpoint" "cnbate_traffic_manager_endpoint02" {
-  name                = "cnbateblogweb_webapp02_performance"
-  resource_group_name = data.azurerm_resource_group.cnbate_resource_group.name
-  profile_name        = azurerm_traffic_manager_profile.cnbate_traffic_manager_profile.name
-  target_resource_id  = azurerm_app_service.cnbate_app_service02.id
-  type                = "azureEndpoints"
+module "cnbate_Web_app" {
+  source                = "../module/web_app"
+  app_service_locations = [local.location_eastAsia, local.location_southeastAsia]
+  resource_group_name   = data.azurerm_resource_group.cnbate_resource_group.name
 
-  geo_mappings = ["SG"]
+  enable                  = var.enable
+  enable_app_service_plan = var.enable_app_service_plan
+  app_service_plan_count  = var.app_service_plan_count
+  app_service_plan_names  = var.app_service_plan_names
+  app_service_plans       = var.app_service_plans
+
+  enable_app_service = var.enable_app_service
+  app_service_count  = var.app_service_count
+  app_service_names  = var.app_service_names
+  app_settings       = var.app_settings
 }
-
-resource "azurerm_app_service_plan" "cnbate_app_service_plan01" {
-  name                = "cnbate_appserviceplan01"
-  location            = local.location_eastAsia
-  resource_group_name = data.azurerm_resource_group.cnbate_resource_group.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-resource "azurerm_app_service_plan" "cnbate_app_service_plan02" {
-  name                = "cnbate_appserviceplan02"
-  location            = local.location_southeastAsia
-  resource_group_name = data.azurerm_resource_group.cnbate_resource_group.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-resource "azurerm_app_service" "cnbate_app_service01" {
-  name                = "CnBateBlogWeb01"
-  location            = local.location_eastAsia
-  resource_group_name = data.azurerm_resource_group.cnbate_resource_group.name
-  app_service_plan_id = azurerm_app_service_plan.cnbate_app_service_plan01.id
-
-  app_settings = {
-    "ASPNETCORE_ENVIRONMENT" = "Production"
-  }
-}
-
-resource "azurerm_app_service" "cnbate_app_service02" {
-  name                = "CnBateBlogWeb02"
-  location            = local.location_southeastAsia
-  resource_group_name = data.azurerm_resource_group.cnbate_resource_group.name
-  app_service_plan_id = azurerm_app_service_plan.cnbate_app_service_plan02.id
-
-  app_settings = {
-    "ASPNETCORE_ENVIRONMENT" = "Production"
-  }
-}
-
-
-
