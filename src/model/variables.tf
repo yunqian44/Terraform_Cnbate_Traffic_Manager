5e710bf2ad95dcea4ba5ccb3d52ee0fccdd4537c
yunqian44 <yunqian8@live.com>
2021-01-24 15:32:50 +0800

    add model description

--- src/model/variables.tf
+++ src/model/variables.tf
@@ -2,26 +2,26 @@
 variable "storage_account_name" {
   type        = string
   default     = "cnbateterraformstorage"
-  description = "(optional) describe your variable"
+  description = "(required)The Azure Storage Account Name of the terraform remote Storage"
 }
 
 variable "container_name" {
   type        = string
   default     = "terraform-state"
-  description = "(optional) describe your variable"
+  description = "(required)The Container Name for the terraform remote storage status file"
 }
 
 variable "key" {
   type        = string
   default     = "cnbate.terraform.stats"
-  description = "(optional) describe your variable"
+  description = "(required)The Container Key Name for terraform the remote storage status file"
 }
 
 ########################## resource group #########################
 variable "resource_group_name" {
   type        = string
   default     = "Web_Test_TF_RG"
-  description = "(optional) describe your variable"
+  description = "(required)The name of the resource group"
 }
 
 ########################## resource group #########################
@@ -29,25 +29,25 @@ variable "resource_group_name" {
 variable "traffic_manager_name" {
   type        = string
   default     = "cnbateblogweb"
-  description = "(optional) describe your variable"
+  description = "(required)The name of the traffic manager profile"
 }
 
 variable "traffic_routing_method" {
   type        = string
   default     = "Geographic"
-  description = "(optional) describe your variable"
+  description = "(required) Specifies the algorithm used to route traffic"
 }
 
 variable "relative_name" {
   type        = string
   default     = "cnbateblogweb"
-  description = "(optional) describe your variable"
+  description = "(required) The relative domain name, this is combined with the domain name used by Traffic Manager to form the FQDN which is exported as documented below."
 }
 
 variable "ttl" {
   type        = number
   default     = 60
-  description = "(optional) describe your variable"
+  description = "(Required) The TTL value of the Profile used by Local DNS resolvers and clients"
 }
 
 variable "tags" {
@@ -55,19 +55,19 @@ variable "tags" {
   default = {
     Environment = "Production"
   }
-  description = "(optional) describe your variable"
+  description = "(optional) A mapping of tags to assign to the resource."
 }
 
 variable "enable_traffic_manager_endpoint" {
   type        = bool
   default     = true
-  description = "(optional) describe your variable"
+  description = "(required) Whether to create traffic manager endpoint"
 }
 
 variable "traffic_manager_endpoint_count" {
   type        = number
   default     = 2
-  description = "(optional) describe your variable"
+  description = "(required) number of create traffic manager endpoint"
 }
 
 variable "traffic_manager_endpoint_names" {
@@ -76,13 +76,13 @@ variable "traffic_manager_endpoint_names" {
     "cnbateblogweb_webapp01_performance",
     "cnbateblogweb_webapp02_performance"
   ]
-  description = "(optional) describe your variable"
+  description = "(required) The name of the Traffic Manager endpoint."
 }
 
 variable "traffic_manager_endpoint_type" {
   type        = string
   default     = "azureEndpoints"
-  description = "(optional) describe your variable"
+  description = "(required) The Endpoint type, must be one of: 1:azureEndpoints,2:externalEndpoints,3:nestedEndpoints"
 }
 
 variable "geo_mappings" {
@@ -91,26 +91,26 @@ variable "geo_mappings" {
     ["CN"],
     ["SG"]
   ]
-  description = "(optional) describe your variable"
+  description = "(Optional) A list of Geographic Regions used to distribute traffic, such as WORLD, UK or DE. "
 }
 
 ########################## web service app #########################
 variable "enable" {
   type        = bool
   default     = true
-  description = "(optional) describe your variable"
+  description = "(required) Whether to create a web app"
 }
 
 variable "enable_app_service_plan" {
   type        = bool
   default     = true
-  description = "(optional) describe your variable"
+  description = "(required) Whether to create a web app plan"
 }
 
 variable "app_service_plan_count" {
   type        = number
   default     = 2
-  description = "(optional) describe your variable"
+  description = "(required) number of create a web app plan"
 }
 
 variable "app_service_plan_names" {
@@ -119,7 +119,7 @@ variable "app_service_plan_names" {
     "cnbate_appserviceplan01",
     "cnbate_appserviceplan02"
   ]
-  description = "(optional) describe your variable"
+  description = "(required) name of create a web app plan"
 }
 
 variable "app_service_plans" {
@@ -133,19 +133,19 @@ variable "app_service_plans" {
       tier = "Standard",
       size = "S1"
   }]
-  description = "(optional) describe your variable"
+  description = "(required) sku of create a web app plan"
 }
 
 variable "enable_app_service" {
   type        = bool
   default     = true
-  description = "(optional) describe your variable"
+  description = "(required) Whether to create a web app "
 }
 
 variable "app_service_count" {
   type        = number
   default     = 2
-  description = "(optional) describe your variable"
+  description = "(required) number of create a web app."
 }
 
 variable "app_service_names" {
@@ -154,7 +154,7 @@ variable "app_service_names" {
     "CnBateBlogWeb01",
     "CnBateBlogWeb02"
   ]
-  description = "(optional) describe your variable"
+  description = "(required) specifies the name of the app service."
 }
 
 variable "app_settings" {
@@ -169,7 +169,7 @@ variable "app_settings" {
       "Loaction"               = "Southeast Asia"
     }
   ]
-  description = "(optional) describe your variable"
+  description = "(Optional) A key-value pair of app settings."
 }
 
 
